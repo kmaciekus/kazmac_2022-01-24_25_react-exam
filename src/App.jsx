a466ac308d458cc1dd8c0a1cb6816937ce7c4e70
kamciekus <kazimieras.maciekus@gmail.com>
2022-01-25 05:19:51 +0200

    started reg page

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@
 import './App.css';
 import { Form } from "./components/Form";
 import { Navbar } from "./organisms/Navbar";
+import { Register } from "./pages/Register";
 
 function App() {
   return (
     <div className="App">
         <Navbar linkHome="home" linkAdd="add" />
+        <Register />
         <Form register="register"/>
     </div>
   );
